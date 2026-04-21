76b1102d4c105db95780f74f184b550d7de59a8c
gabriel <gb.ribeiropedroso@gmail.com>
2026-04-20 22:05:50 -0300

    tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <p>Olá mundo</p>
+   <RouterLink to="/about">About</RouterLink>
 </template>
